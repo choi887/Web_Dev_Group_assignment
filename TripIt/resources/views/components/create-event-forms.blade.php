5ba1c98e2575ce70d353ca5e7f2e4db559978ddd
choi887 <154850973+choi887@users.noreply.github.com>
2024-07-17 17:43:16 +0800

    Update create-event-forms.blade.php

--- TripIt/resources/views/components/create-event-forms.blade.php
+++ TripIt/resources/views/components/create-event-forms.blade.php
@@ -53,6 +53,22 @@
                         required="" min="0" placeholder="Example: 10">
                 </div>
                 <div class="sm:col-span-4">
+                    <label for="address" class="block mb-2 text-sm font-medium text-black">Address</label>
+                    <div class="relative max-w-sm">
+                        <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                class="size-6">
+                                <path fill-rule="evenodd"
+                                    d="m11.54 22.351.07.04.028.016a.76.76 0 0 0 .723 0l.028-.015.071-.041a16.975 16.975 0 0 0 1.144-.742 19.58 19.58 0 0 0 2.683-2.282c1.944-1.99 3.963-4.98 3.963-8.827a8.25 8.25 0 0 0-16.5 0c0 3.846 2.02 6.837 3.963 8.827a19.58 19.58 0 0 0 2.682 2.282 16.975 16.975 0 0 0 1.145.742ZM12 13.5a3 3 0 1 0 0-6 3 3 0 0 0 0 6Z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </div>
+                        <input id="address" name="address" type="text"
+                            class="border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 "
+                            placeholder="Enter Your Address here">
+                    </div>
+                </div>
+                <div class="sm:col-span-4">
                     <label for="start_date" class="block mb-2 text-sm font-medium text-black">Start Date</label>
                     <div class="relative max-w-sm">
                         <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
