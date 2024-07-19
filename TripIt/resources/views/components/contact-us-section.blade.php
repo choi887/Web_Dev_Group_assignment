3183c33fc5337967e533cd40127f0ab2f38c1a50
QiiZheee <155406775+QiiZheee@users.noreply.github.com>
2024-07-19 14:26:21 +0800

    Update contact-us-section.blade.php

--- TripIt/resources/views/components/contact-us-section.blade.php
+++ TripIt/resources/views/components/contact-us-section.blade.php
@@ -19,8 +19,8 @@
                             fill="url(#83fd4e5a-9d52-42fc-97b6-718e5d7ee527)" />
                     </svg>
                 </div>
-                <h2 class="text-3xl font-bold tracking-tight text-gray-900">Get in touch</h2>
-                <p class="mt-6 text-lg leading-8 text-gray-600">Got a technical issue? Want to send feedback
+                <h2 class="text-3xl font-bold tracking-tight button-text-color">Get in touch</h2>
+                <p class="mt-6 text-lg leading-8 ">Got a technical issue? Want to send feedback
                     about a beta feature? Need details about our Business plan? Let us know.</p>
                 <dl class="mt-10 space-y-4 text-base leading-7 text-gray-600">
                     <div class="flex gap-x-4">
@@ -61,11 +61,11 @@
                 </dl>
             </div>
         </div>
-        <form action="#" method="POST" class="px-6 pb-14 pt-10">
+        <form action="#" class="px-6 pb-14 pt-10">
             <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
                 <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                     <div>
-                        <label for="first-name" class="block text-sm font-semibold leading-6 text-gray-900">First
+                        <label for="first-name" class="block text-sm font-semibold leading-6 button-text-color">First
                             name</label>
                         <div class="mt-2.5">
                             <input type="text" name="first-name" id="first-name" autocomplete="given-name"
@@ -73,7 +73,7 @@
                         </div>
                     </div>
                     <div>
-                        <label for="last-name" class="block text-sm font-semibold leading-6 text-gray-900">Last
+                        <label for="last-name" class="block text-sm font-semibold leading-6 button-text-color">Last
                             name</label>
                         <div class="mt-2.5">
                             <input type="text" name="last-name" id="last-name" autocomplete="family-name"
@@ -81,14 +81,15 @@
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+                        <label for="email"
+                            class="block text-sm font-semibold leading-6 button-text-color">Email</label>
                         <div class="mt-2.5">
                             <input type="email" name="email" id="email" autocomplete="email"
                                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6">
                         </div>
                     </div>
                     <div class="sm:col-span-2">
-                        <label for="phone-number" class="block text-sm font-semibold leading-6 text-gray-900">Phone
+                        <label for="phone-number" class="block text-sm font-semibold leading-6 button-text-color">Phone
                             number</label>
                         <div class="mt-2.5">
                             <input type="tel" name="phone-number" id="phone-number" autocomplete="tel"
@@ -97,7 +98,7 @@
                     </div>
                     <div class="sm:col-span-2">
                         <label for="message"
-                            class="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+                            class="block text-sm font-semibold leading-6 button-text-color">Message</label>
                         <div class="mt-2.5">
                             <textarea name="message" id="message" rows="4"
                                 class="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"></textarea>
@@ -105,7 +106,7 @@
                     </div>
                 </div>
                 <div class="mt-8 flex justify-end">
-                    <button type="submit"
+                    <button
                         class="rounded-md bg-blue-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">Send
                         message</button>
                 </div>
