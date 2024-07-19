79d3717d13e7146fafd3a66a10dec55983e9e748
choi887 <154850973+choi887@users.noreply.github.com>
2024-07-19 11:02:26 +0800

    front end changes

--- TripIt/resources/views/components/sidebar-create.blade.php
+++ TripIt/resources/views/components/sidebar-create.blade.php
@@ -1,11 +1,11 @@
 <aside id="sidebar-multi-level-sidebar"
     class="relative hidden sm:block w-64  transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
-    <div class="h-full px-3 py-4 overflow-y-auto bg-gradient-to-r from-cyan-100 to-sky-200">
+    <div class="h-full px-3 py-4 overflow-y-auto bg-gradient-to-r from-cyan-200 to-sky-300">
         <ul class="space-y-2 font-medium">
             <li>
                 <a href="{{ route('dashboard') }}"
-                    class="flex items-center p-2 text-gray-900 rounded-lg hover:bg-gradient-to-r from-sky-200 to-sky-300 group">
-                    <svg class="w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
+                    class="flex items-center p-2 text-gray-800 rounded-lg  hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group">
+                    <svg class="w-5 h-5 text-gray-700 transition duration-75  group-hover:text-blue-700 "
                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
                         <path
                             d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z" />
@@ -17,9 +17,9 @@
             </li>
             <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gradient-to-r from-sky-200 to-sky-300"
+                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group  hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group"
                     aria-controls="dropdown-example" data-collapse-toggle="dropdown-example">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 "
+                    <svg class="flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 "
                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
                         <path
                             d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
@@ -31,12 +31,12 @@
                             d="m1 1 4 4 4-4" />
                     </svg>
                 </button>
-                <ul id="dropdown-example" class="hidden py-2 space-y-2">
+                <ul id="dropdown-example" class="hidden py-2 space-y-2 ">
                     <li>
                         <a href="{{ route('event.list.admin') }}"
-                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gradient-to-r from-sky-200 to-sky-300">
+                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group   hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class=" flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 ">
+                                class=" flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 ">
                                 <path fill-rule="evenodd"
                                     d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 18.375V5.625ZM21 9.375A.375.375 0 0 0 20.625 9h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5Zm0 3.75a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5Zm0 3.75a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5ZM10.875 18.75a.375.375 0 0 0 .375-.375v-1.5a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5ZM3.375 15h7.5a.375.375 0 0 0 .375-.375v-1.5a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375Zm0-3.75h7.5a.375.375 0 0 0 .375-.375v-1.5A.375.375 0 0 0 10.875 9h-7.5A.375.375 0 0 0 3 9.375v1.5c0 .207.168.375.375.375Z"
                                     clip-rule="evenodd" />
@@ -48,9 +48,9 @@
                     </li>
                     <li>
                         <a href="{{ route('event.create') }}"
-                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gradient-to-r from-sky-200 to-sky-300">
+                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group   hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 ">
+                                class="flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 ">
                                 <path fill-rule="evenodd"
                                     d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875ZM12.75 12a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V18a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V12Z"
                                     clip-rule="evenodd" />
@@ -68,14 +68,18 @@
             <!-- event sidebar end -->
             <li>
                 <button type="button"
-                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gradient-to-r from-sky-200 to-sky-300"
+                    class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group  hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group"
                     aria-controls="dropdown-package" data-collapse-toggle="dropdown-package">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 "
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 21">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                        class="flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 ">
                         <path
-                            d="M15 12a1 1 0 0 0 .962-.726l2-7A1 1 0 0 0 17 3H3.77L3.175.745A1 1 0 0 0 2.208 0H1a1 1 0 0 0 0 2h.438l.6 2.255v.019l2 7 .746 2.986A3 3 0 1 0 9 17a2.966 2.966 0 0 0-.184-1h2.368c-.118.32-.18.659-.184 1a3 3 0 1 0 3-3H6.78l-.5-2H15Z" />
+                            d="M3.375 3C2.339 3 1.5 3.84 1.5 4.875v.75c0 1.036.84 1.875 1.875 1.875h17.25c1.035 0 1.875-.84 1.875-1.875v-.75C22.5 3.839 21.66 3 20.625 3H3.375Z" />
+                        <path fill-rule="evenodd"
+                            d="m3.087 9 .54 9.176A3 3 0 0 0 6.62 21h10.757a3 3 0 0 0 2.995-2.824L20.913 9H3.087Zm6.163 3.75A.75.75 0 0 1 10 12h4a.75.75 0 0 1 0 1.5h-4a.75.75 0 0 1-.75-.75Z"
+                            clip-rule="evenodd" />
                     </svg>
-                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Package</span>
+
+                    <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">Packages</span>
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
                         viewBox="0 0 10 6">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -85,9 +89,9 @@
                 <ul id="dropdown-package" class="hidden py-2 space-y-2">
                     <li>
                         <a href="{{ route('package.list.admin') }}"
-                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gradient-to-r from-cyan-200 to-sky-400">
+                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group   hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class=" flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 ">
+                                class=" flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 ">
                                 <path fill-rule="evenodd"
                                     d="M1.5 5.625c0-1.036.84-1.875 1.875-1.875h17.25c1.035 0 1.875.84 1.875 1.875v12.75c0 1.035-.84 1.875-1.875 1.875H3.375A1.875 1.875 0 0 1 1.5 18.375V5.625ZM21 9.375A.375.375 0 0 0 20.625 9h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5Zm0 3.75a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5Zm0 3.75a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5a.375.375 0 0 0 .375-.375v-1.5ZM10.875 18.75a.375.375 0 0 0 .375-.375v-1.5a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375h7.5ZM3.375 15h7.5a.375.375 0 0 0 .375-.375v-1.5a.375.375 0 0 0-.375-.375h-7.5a.375.375 0 0 0-.375.375v1.5c0 .207.168.375.375.375Zm0-3.75h7.5a.375.375 0 0 0 .375-.375v-1.5A.375.375 0 0 0 10.875 9h-7.5A.375.375 0 0 0 3 9.375v1.5c0 .207.168.375.375.375Z"
                                     clip-rule="evenodd" />
@@ -99,9 +103,9 @@
                     </li>
                     <li>
                         <a href="{{ route('package.create') }}"
-                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group hover:bg-gradient-to-r from-cyan-200 to-sky-400">
+                            class="flex items-center p-2 text-gray-900 transition duration-75 rounded-lg group   hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 group-hover:text-gray-900 ">
+                                class="flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 ">
                                 <path fill-rule="evenodd"
                                     d="M5.625 1.5H9a3.75 3.75 0 0 1 3.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 0 1 3.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 0 1-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875ZM12.75 12a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V18a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V12Z"
                                     clip-rule="evenodd" />
@@ -131,8 +135,8 @@
             </li> --}}
             <li>
                 <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gradient-to-r from-cyan-200 to-sky-400 group">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 "
+                    class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gradient-to-r from-cyan-200 hover:text-blue-700 to-sky-400 group">
+                    <svg class="flex-shrink-0 w-5 h-5 text-gray-600 transition duration-75 group-hover:text-blue-700 "
                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
                         <path
                             d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z" />
@@ -140,18 +144,7 @@
                     <span class="flex-1 ms-3 whitespace-nowrap">Users</span>
                 </a>
             </li>
-            <li>
-                <a href="#"
-                    class="flex items-center p-2 text-gray-900 rounded-lg  hover:bg-gradient-to-r from-cyan-200 to-sky-400 group">
-                    <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75  group-hover:text-gray-900 "
-                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
-                        viewBox="0 0 18 20">
-                        <path
-                            d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z" />
-                    </svg>
-                    <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
-                </a>
-            </li>
+
 
         </ul>
     </div>
--- TripIt/resources/views/dashboard.blade.php
+++ TripIt/resources/views/dashboard.blade.php
@@ -14,15 +14,16 @@
             <x-sidebar-create></x-sidebar-create>
             <div class="max-w-screen-xl w-full mt-4  lg:px-12 overflow-hidden">
                 <!-- content container start -->
-                <div class="  grid w-full grid-cols-1 gap-4 mt-4 xl:grid-cols-2 2xl:grid-cols-3">
-                    <div class="items-center justify-between p-4   rounded-lg shadow-xl sm:flex  sm:p-6 ">
+                <div class=" grid w-full grid-cols-1 gap-4 mt-4 xl:grid-cols-2 2xl:grid-cols-3">
+                    <!-- Card with graph start -->
+                    <div class="items-center justify-between p-4 rounded-lg shadow-xl sm:flex  sm:p-6 ">
                         <div class="w-full">
                             <h3 class="text-base font-semibold text-gray-900">
-                                New products
+                                Sales this month
                             </h3>
                             <span class="text-2xl font-bold leading-none text-black sm:text-3xl ">2,340</span>
                             <p class="flex items-center text-base font-normal text-gray-500 ">
-                                <span class="flex items-center mr-1.5 text-md text-green-400 ">
+                                <span class="flex items-center mr-1.5 text-md font-semibold text-green-400 ">
                                     <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
                                         xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                                         <path clip-rule="evenodd" fill-rule="evenodd"
@@ -46,6 +47,66 @@
                             </div>
                         </div>
                     </div>
+                    <div class="items-center justify-between p-4 rounded-lg shadow-xl sm:flex  sm:p-6 ">
+                        <div class="w-full">
+                            <h3 class="text-base font-semibold text-gray-900">
+                                Sales this month
+                            </h3>
+                            <span class="text-2xl font-bold leading-none text-black sm:text-3xl ">2,340</span>
+                            <p class="flex items-center text-base font-normal text-gray-500 ">
+                                <span class="flex items-center mr-1.5 text-md font-semibold text-green-400 ">
+                                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                        <path clip-rule="evenodd" fill-rule="evenodd"
+                                            d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z">
+                                        </path>
+                                    </svg>
+                                    12.5%
+                                </span>
+                                Since last month
+                            </p>
+                        </div>
+                        <div class="chart-container">
+                            <svg class="chart" viewBox="0 0 100 60">
+                                <path class="line developer" d="M0,30 Q25,35 50,25 T100,30" />
+                                <path class="line designer" d="M0,45 Q25,15 50,35 T100,10" />
+                            </svg>
+                        </div>
+                    </div>
+                    <div class="items-center justify-between p-4 rounded-lg shadow-xl sm:flex  sm:p-6 ">
+                        <div class="w-full">
+                            <h3 class="text-base font-semibold text-gray-900">
+                                Sales this month
+                            </h3>
+                            <span class="text-2xl font-bold leading-none text-black sm:text-3xl ">2,340</span>
+                            <p class="flex items-center text-base font-normal text-gray-500 ">
+                                <span class="flex items-center mr-1.5 text-md font-semibold text-green-400 ">
+                                    <svg class="w-4 h-4" fill="currentColor" viewBox="0 0 20 20"
+                                        xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                                        <path clip-rule="evenodd" fill-rule="evenodd"
+                                            d="M10 17a.75.75 0 01-.75-.75V5.612L5.29 9.77a.75.75 0 01-1.08-1.04l5.25-5.5a.75.75 0 011.08 0l5.25 5.5a.75.75 0 11-1.08 1.04l-3.96-4.158V16.25A.75.75 0 0110 17z">
+                                        </path>
+                                    </svg>
+                                    12.5%
+                                </span>
+                                Since last month
+                            </p>
+                        </div>
+                        <div class="mt-4">
+                            <div class="flex items-end space-x-2">
+                                <div class="h-10 w-4 bg-blue-600 shadow-xl border border-blue-400"></div>
+                                <div class="h-14 w-4 bg-blue-600"></div>
+                                <div class="h-8 w-4 bg-blue-600"></div>
+                                <div class="h-6 w-4 bg-blue-600"></div>
+                                <div class="h-10 w-4 bg-blue-600"></div>
+                                <div class="h-14 w-4 bg-blue-600"></div>
+                                <div class="h-8 w-4 bg-blue-600"></div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Card with graph end -->
+
                 </div>
 
             </div>
@@ -53,7 +114,6 @@
 
         </div>
         </div>
-
 
     </section>
 </x-app-layout>
